ea52eacc3bf942ddcd156a112c73d56de589d0e7
popo gangnam <pp811010@gmail.com>
2024-03-11 13:43:10 +0700

    test

--- logout.php
+++ logout.php
@@ -3,6 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start(); // Start the session
 session_destroy(); 
-header("Location: index.php"); // Redirect to login page after logout
+header("Location: index.php"); 
 exit;
 ?>
