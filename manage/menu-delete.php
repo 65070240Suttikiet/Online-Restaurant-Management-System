dd18815e32f01ed77c6b3d78c260415d80cc9d12
popo gangnam <pp811010@gmail.com>
2024-03-12 20:13:43 +0700

    .

--- manage/menu-delete.php
+++ manage/menu-delete.php
@@ -1,44 +1,39 @@
 <?php
-class MyDB extends SQLite3
-{
-  function __construct()
+  class MyDB extends SQLite3
   {
-    $this->open('../db/omakase.db');
+    function __construct()
+    {
+      $this->open('../db/omakase.db');
+    }
   }
-}
 
-// 2. Open Database 
-$db = new MyDB();
-if (!$db) {
-  echo $db->lastErrorMsg();
-}
+  $db = new MyDB();
+  if (!$db) {
+    echo $db->lastErrorMsg();
+  }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=ds, initial-scale=1.0">
     <title>Document</title>
-</head>
+  </head>
 
-<body>
+  <body>
     <?php
     $menu_name = $_GET['menu_name'];
-
-
     $sql = "DELETE FROM menu WHERE menu_name = '$menu_name'";
     $sql1 = "DELETE FROM detail_course_menu WHERE menu_name = '$menu_name'";
     if ($db->query($sql) == TRUE & $db->query($sql1) == TRUE) {
-        echo "<script>alert('การลบเมนูสำเร็จ');</script>";
-        echo "<script>window.location.href = 'home-menu.php';</script>";
-
-    }else {
-        echo "Error updating record";
+      echo "<script>alert('การลบเมนูอาหารสำเร็จ');</script>";
+      echo "<script>window.location.href = 'home-menu.php';</script>";
+    } else {
+      echo "<script>alert('การลบเมนูอาหารไม่สำเร็จ');</script>";
+      echo "<script>window.location.href = 'home-menu.php';</script>";
     }
     ?>
-</body>
-
+  </body>
 </html>
